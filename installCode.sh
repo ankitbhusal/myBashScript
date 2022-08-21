1157d7c2358b63ba20143e13e9ad659366c6a2a5
Ankit Bhusal <ankitbhusal95@gmail.com>
2022-08-21 17:27:20 +0545

    install vs code

---
+++ installCode.sh
@@ -0,0 +1,17 @@
+# shell script to install vscode
+#check if wget is intall and install if not
+if ! [ -x "$(command -v wget)" ]; then
+  echo 'Error: wget is not installed.' >&2
+  sudo apt install wget
+  exit 1
+fi
+
+# install vscode using wget
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
